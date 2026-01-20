767dc1fae948caf89ae806738d9fe1cd0c7a836d
anderson <anderson@2wav.com>
2026-01-19 22:26:47 -0700

    MPL 2.0

--- src/ld.d.ts
+++ src/ld.d.ts
@@ -1,4 +1,12 @@
 /**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2026 2wav, Inc.
+ */
+
+/**
  * LD Class TypeScript Declarations
  * JSON-LD utilities for BOLD project
  */
